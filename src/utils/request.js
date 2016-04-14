153c50f0584a90ed5733c265af26b6f668b497a3
Jeremy Bueler <jbueler@gmail.com>
2016-04-13 21:41:34 -0700

    passing only the response through the reject callback

--- src/utils/request.js
+++ src/utils/request.js
@@ -92,7 +92,7 @@ module.exports = function(options, transform) {
           }
         }
         if (response && response.statusCode >= 400 && response.statusCode < 600) {
-          return reject(errorify(body || response, extend({ statusCode: response.statusCode, url: response.req.path}, response.headers)));
+          return reject(JSON.stringify(response));
         }
         return resolve(body);
       });
